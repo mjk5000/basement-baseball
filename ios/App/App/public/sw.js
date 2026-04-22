142b4890d8d6a6819936e7b6d2d4cce0b21c9b64
Kajewski, Matthew <matthew.kajewski@corteva.com>
2026-04-22 14:33:54 -0500

    v1.18.43: Test version bump to verify automatic service worker updates from v1.18.42

--- ios/App/App/public/sw.js
+++ ios/App/App/public/sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Basement Baseball PWA
-const CACHE_VERSION = 'v1.18.42';
+const CACHE_VERSION = 'v1.18.43';
 const CACHE_NAME = `basement-baseball-${CACHE_VERSION}`;
 
 const urlsToCache = [
