f96d5ac244228058863fc0a53752dc12f4f95f98
Kajewski, Matthew <matthew.kajewski@corteva.com>
2026-04-23 14:24:17 -0500

    v1.18.61: Add custom PWA icons - green baseball diamond with green B

--- www/sw.js
+++ www/sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Basement Baseball PWA
-const CACHE_VERSION = 'v1.18.60';
+const CACHE_VERSION = 'v1.18.61';
 const CACHE_NAME = `basement-baseball-${CACHE_VERSION}`;
 
 const urlsToCache = [
